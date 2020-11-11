14cd5d0ea1520d3d9fbce7eb956754ac3a982696
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-11-12 01:41:56 +0300

    Add CategoriesViewModel

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CategoriesFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CategoriesFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.getflix.R
+import com.example.getflix.adapters.SubcategoryAdapter
 import com.example.getflix.databinding.FragmentCategoriesBinding
-import com.example.getflix.databinding.FragmentHomePageBinding
+
 
 
 class CategoriesFragment : Fragment() {
+    private lateinit var categoriesViewModel: CategoriesViewModel
 
 
     override fun onCreateView(
@@ -20,9 +24,13 @@ class CategoriesFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentCategoriesBinding>(inflater,R.layout.fragment_categories,
             container,false)
-
+        categoriesViewModel =  ViewModelProvider(this).get(CategoriesViewModel::class.java)
+        val adapter = SubcategoryAdapter()
+        categoriesViewModel.displayedCategories.observe(viewLifecycleOwner, Observer {
+            it.let {
+                adapter.submitList(it["woman"])
+            }
+        })
         return binding.root
     }
-
-
 }
